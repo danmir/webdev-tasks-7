8dc60df0ab08f7cf93064f8256f18701828e8d32
danmir <danmir@me.com>
2016-04-28 21:27:55 +0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    // devtool: 'cheap-module-eval-source-map',
     entry: [
         'webpack-hot-middleware/client',
         './bundles/page/page'
